396f5cc0e5d4b9ccdc108236276a32d4f7b3cddf
Isabel <isgrator@gmail.com>
2019-06-26 23:33:26 +0200

    Ejercicio: Conectividad básica con Nearby Connections

--- mobile/src/main/java/com/cursoandroid/nearbyconnections/MainActivity.java
+++ mobile/src/main/java/com/cursoandroid/nearbyconnections/MainActivity.java
@@ -1,13 +1,197 @@
 package com.cursoandroid.nearbyconnections;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.connection.ConnectionInfo;
+import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback;
+import com.google.android.gms.nearby.connection.ConnectionResolution;
+import com.google.android.gms.nearby.connection.ConnectionsStatusCodes;
+import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo;
+import com.google.android.gms.nearby.connection.DiscoveryOptions;
+import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback;
+import com.google.android.gms.nearby.connection.Payload;
+import com.google.android.gms.nearby.connection.PayloadCallback;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
+import com.google.android.gms.nearby.connection.Strategy;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION=1;
+    // Consejo: utiliza como SERVICE_ID el nombre de tu paquete
+    private static final String SERVICE_ID = "com.cursoandroid.nearbyconnections";
+    private static final String TAG = "Mobile:";
+    Button botonLED;
+    TextView textview;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textview = (TextView) findViewById(R.id.textView1);
+        botonLED = (Button) findViewById(R.id.buttonLED);
+
+        botonLED.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.i(TAG, "Boton presionado");
+                startDiscovery();
+                textview.setText("Buscando...");
+            }
+        });
+
+        // Comprobación de permisos peligrosos. Se pide permiso al usuario en tiempo de ejecución
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+        }
+    }
+
+    // Gestión de permisos
+    @Override  public void onRequestPermissionsResult(int requestCode,
+                                                      String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i(TAG, "Permisos concedidos");
+                } else {
+                    Log.i(TAG, "Permisos denegados");
+                    textview.setText("Debe aceptar los permisos para comenzar");
+                    botonLED.setEnabled(false);
+                }
+                return;
+            }
+        }
+    }
+
+    private void startDiscovery() {
+        Nearby.getConnectionsClient(this).startDiscovery(SERVICE_ID,
+                mEndpointDiscoveryCallback, new DiscoveryOptions(Strategy.P2P_STAR))
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override public void onSuccess(Void unusedResult) {
+                        Log.i(TAG, "Estamos en modo descubrimiento!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "Modo descubrimiento no iniciado.", e);
+                    }
+                });
+    }
+
+    private void stopDiscovery() {
+        Nearby.getConnectionsClient(this).stopDiscovery();
+        Log.i(TAG, "Se ha detenido el modo descubrimiento.");
+    }
+
+    //Gestiona el proceso de descubrimiento de anunciantes y la estrategia a seguir.
+    //Se invoca cada vez que detecta un anunciante o que desaparece uno
+    private final EndpointDiscoveryCallback mEndpointDiscoveryCallback =     new EndpointDiscoveryCallback() {
+
+        //Se lanza cuando detecta un anunciante
+        @Override public void onEndpointFound(String endpointId, DiscoveredEndpointInfo discoveredEndpointInfo) {
+            Log.i(TAG, "Descubierto dispositivo con Id: " + endpointId);
+            String discoveredPointName = "Descubierto: " +discoveredEndpointInfo.getEndpointName();
+            textview.setText(discoveredPointName);
+            stopDiscovery();
+            // Iniciamos la conexión con al anunciante "Nearby LED"
+            Log.i(TAG, "Conectando...");
+            Nearby.getConnectionsClient(getApplicationContext())
+                    .requestConnection("Nearby LED",endpointId,mConnectionLifecycleCallback)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override public void onSuccess(Void unusedResult) {
+                            Log.i(TAG, "Solicitud lanzada, falta que ambos " +
+                                    "lados acepten");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override public void onFailure(@NonNull Exception e) {
+                            Log.e(TAG, "Error en solicitud de conexión", e);
+                            textview.setText("Desconectado");
+                        }
+                    });
+        }
+
+        //Se lanza cuando se pierde un anunciante
+        @Override public void onEndpointLost(String endpointId) {}
+    };
+
+    //Se invoca durante el proceso de conexión con el anunciante
+    private final ConnectionLifecycleCallback mConnectionLifecycleCallback =
+            new ConnectionLifecycleCallback() {
+
+                @Override public void onConnectionInitiated(
+                        String endpointId, ConnectionInfo connectionInfo) {
+                    // Aceptamos la conexión automáticamente en ambos lados.
+                    Log.i(TAG, "Aceptando conexión entrante sin autenticación");
+                    Nearby.getConnectionsClient(getApplicationContext())
+                            .acceptConnection(endpointId, mPayloadCallback);
+                }
+
+                //Se invoca cuando eporceso de conexión ha finalizado
+                @Override public void onConnectionResult(String endpointId,
+                                                         ConnectionResolution result) {
+                    switch (result.getStatus().getStatusCode()) {
+                        case ConnectionsStatusCodes.STATUS_OK:
+                            Log.i(TAG, "Estamos conectados!");
+                            textview.setText("Conectado");
+                            //La conexión es correcta. sendData() para enviar el mensaje “ACCION” a la RPi3
+                            sendData(endpointId, "SWITCH");
+                            break;
+                        case ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED:
+                            Log.i(TAG, "Conexión rechazada por uno o ambos lados");
+                            textview.setText("Desconectado");
+                            break;
+                        case ConnectionsStatusCodes.STATUS_ERROR:
+                            Log.i(TAG, "Conexión perdida antes de poder ser aceptada");
+                            textview.setText("Desconectado");
+                            break;
+                    }
+                }
+
+                @Override public void onDisconnected(String endpointId) {
+                    Log.i(TAG, "Desconexión del endpoint, no se pueden " +
+                            "intercambiar más datos.");
+                    textview.setText("Desconectado");
+                }
+            };
+
+    //Se invoca cuando se reciben datos del anunciante
+    private final PayloadCallback mPayloadCallback = new PayloadCallback() {
+        // En este ejemplo, el móvil no recibirá transmisiones de la RP3
+        @Override public void onPayloadReceived(String endpointId, Payload payload) {
+            // Payload recibido
+        }
+
+        @Override public void onPayloadTransferUpdate(String endpointId,PayloadTransferUpdate update) {
+            // Actualizaciones sobre el proceso de transferencia
+        }
+    };
+
+    private void sendData(String endpointId, String mensaje) {
+        textview.setText("Transfiriendo...");
+        Payload data = null;
+        try {
+            data = Payload.fromBytes(mensaje.getBytes("UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            Log.e(TAG, "Error en la codificación del mensaje.", e);
+        }
+        Nearby.getConnectionsClient(this).sendPayload(endpointId, data);
+        Log.i(TAG, "Mensaje enviado.");
     }
 }
--- things/src/main/java/com/cursoandroid/nearbyconnections/MainActivity.java
+++ things/src/main/java/com/cursoandroid/nearbyconnections/MainActivity.java
@@ -3,6 +3,25 @@ package com.cursoandroid.nearbyconnections;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.connection.AdvertisingOptions;
+import com.google.android.gms.nearby.connection.ConnectionInfo;
+import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback;
+import com.google.android.gms.nearby.connection.ConnectionResolution;
+import com.google.android.gms.nearby.connection.ConnectionsStatusCodes;
+import com.google.android.gms.nearby.connection.Payload;
+import com.google.android.gms.nearby.connection.PayloadCallback;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
+import com.google.android.gms.nearby.connection.Strategy;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.PeripheralManager;
+
+import java.io.IOException;
 
 /**
  * Skeleton of an Android Things activity.
@@ -24,9 +43,146 @@ import android.os.Bundle;
  * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
  */
 public class MainActivity extends Activity {
+    // Consejo: utiliza como SERVICE_ID el nombre de tu paquete
+    private static final String SERVICE_ID = "com.cursoandroid.nearbyconnections";
+    private static final String TAG = "Things:";
+    private final String PIN_LED = "BCM17"; //"BCM18"; //Pin de salida
+    public Gpio mLedGpio;
+    private Boolean ledStatus;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Configuración del LED
+        ledStatus = false;
+        PeripheralManager service = PeripheralManager.getInstance();
+        try {
+            mLedGpio = service.openGpio(PIN_LED);
+            mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+        } catch (IOException e) {
+            Log.e(TAG, "Error en el API PeripheralIO", e);
+        }
+        // Arrancamos modo anunciante
+        startAdvertising();
+    }
+
+    private void startAdvertising() {
+        Nearby.getConnectionsClient(this).startAdvertising(
+                "Nearby LED", SERVICE_ID, mConnectionLifecycleCallback,
+                new AdvertisingOptions(Strategy.P2P_STAR))
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override public void onSuccess(Void unusedResult) {
+                        Log.i(TAG, "Estamos en modo anunciante!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "Error al comenzar el modo anunciante", e);
+                    }
+                });
+    }
+
+    private void stopAdvertising() {
+        Nearby.getConnectionsClient(this).stopAdvertising();
+        Log.i(TAG, "Detenido el modo anunciante!");
+    }
+
+    //Gestiona todo el proceso de peticiones de conexión que se reciban y la estrategia a utilizar.
+    //Se llama cuando un descubridor intenta conectarse con "nosotros".
+    private final ConnectionLifecycleCallback mConnectionLifecycleCallback =
+            new ConnectionLifecycleCallback() {
+                @Override public void onConnectionInitiated(
+                        String endpointId, ConnectionInfo connectionInfo) {
+                    //Lugar ideal para hacer un proceso de autenticación *************
+                    //****************************************************************
+                    // Aceptamos la conexión automáticamente en ambos lados.
+                    Nearby.getConnectionsClient(getApplicationContext())
+                            .acceptConnection(endpointId, mPayloadCallback);
+                    Log.i(TAG, "Aceptando conexión entrante sin autenticación");
+                }
+
+                @Override public void onConnectionResult(String endpointId,
+                                                         ConnectionResolution result) {
+                    switch (result.getStatus().getStatusCode()) {
+                        case ConnectionsStatusCodes.STATUS_OK:
+                            Log.i(TAG, "Estamos conectados!");
+                            stopAdvertising();
+                            break;
+                        case ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED:
+                            Log.i(TAG, "Conexión rechazada por uno o ambos lados");
+                            break;
+                        case ConnectionsStatusCodes.STATUS_ERROR:
+                            Log.i(TAG, "Conexión perdida antes de ser aceptada");
+                            break;
+                    }
+                }
+
+                @Override
+                public void onDisconnected(String endpointId) {
+                    Log.i(TAG, "Desconexión del endpoint, no se pueden " +
+                            "intercambiar más datos.");
+                    startAdvertising();
+                }
+            };
+
+    //Se invoca cuando se reciben datos por una conexión establecida con un dispositivo
+    private final PayloadCallback mPayloadCallback = new PayloadCallback() {
+        //Recibe la carga tipo BYTE que hemos enviado desde el móvil.
+        @Override public void onPayloadReceived(String endpointId, Payload payload) {
+            String message = new String(payload.asBytes());
+            Log.i(TAG, "Se ha recibido una transferencia desde (" +
+                    endpointId + ") con el siguiente contenido: " + message);
+            disconnect(endpointId);  //Cierra nuestro lado de la conexión
+            switch (message) {
+                case "SWITCH":
+                    switchLED();  //Creo que esto equivale a doRemoteAction() de los apuntes (U5, pág. 32)
+                    break;
+                default:
+                    Log.w(TAG, "No existe una acción asociada a este " +
+                            "mensaje.");
+                    break;
+            }
+        }
+
+        @Override public void onPayloadTransferUpdate(String endpointId, PayloadTransferUpdate update) {
+            // Actualizaciones sobre el proceso de transferencia. Sólo útil cuando los tipos de datos son FILE
+        }
+    };
+
+    public void switchLED() {
+        try {
+            if (ledStatus) {
+                mLedGpio.setValue(false);
+                ledStatus = false;
+                Log.i(TAG, "LED OFF");
+            } else {
+                mLedGpio.setValue(true);
+                ledStatus = true;
+                Log.i(TAG, "LED ON");
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Error en el API PeripheralIO", e);
+        }
+    }
+
+    protected void disconnect(String endpointId) {
+        Nearby.getConnectionsClient(this)
+                .disconnectFromEndpoint(endpointId);
+        Log.i(TAG, "Desconectado del endpoint (" + endpointId + ").");
+        startAdvertising();
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onDestroy() {
+        super.onDestroy();
+        stopAdvertising();
+        if (mLedGpio != null) {
+            try {
+                mLedGpio.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error en el API PeripheralIO", e);
+            } finally {
+                mLedGpio = null;
+            }
+        }
     }
 }
